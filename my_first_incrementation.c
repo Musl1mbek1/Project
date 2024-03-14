9232af373946febd13468aeca314a1bd09bd3192
Musl1mbek1 <156788704+Musl1mbek1@users.noreply.github.com>
2024-03-14 17:50:54 +0500

    Create my_first_incrementation.c

---
+++ my_first_incrementation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+ int my_index = 0;
+ // replace this comment with an increment
+ my_index++;
+ printf("%d\n", my_index);
+ // replace this comment with an decrement
+ my_index--;
+ printf("%d\n", my_index);
+ // replace this comment with an increment
+ my_index++;
+ printf("%d\n", my_index);
+ return 0;
+}
