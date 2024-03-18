4bde996f049865252f8bc099ccb53a6859a388bc
Musl1mbek1 <156788704+Musl1mbek1@users.noreply.github.com>
2024-03-18 09:10:59 +0500

    Create my_print_reverse_alphabet.c

---
+++ my_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void my_putchar(char c) {
+    write(1, &c, 1);
+}
+
+void my_print_reverse_alphabet() {
+    char letter = 'z';
+    while (letter >= 'a') {
+        my_putchar(letter);
+        letter--;
+    }
+    my_putchar('\n');
+}
+int main() {
+    my_print_reverse_alphabet();
+    return 0;
+}
